19e48bd6de5c2b6e0d5b97f2799509441bea4272
Charlie-Steer <charliesteerart@gmail.com>
2025-12-19 15:00:36 +0100

    Finished cpp01_ex04.

--- cpp01/ex04/main.cpp
+++ cpp01/ex04/main.cpp
@@ -1,5 +1,7 @@
 #include <fstream>
 #include <iostream>
+#include <sstream>
+#include <string>
 
 void error(std::string message) {
 	std::cerr << "Error: " << message << "\n";
@@ -9,16 +11,60 @@ void log(std::string message) {
 	std::cerr << message << "\n";
 }
 
+void log_format() {
+	log("Format: sedlike <filename> <s1> <s2>");
+}
+
 int main(int argc, char *argv[]) {
 	if (argc != 4) {
 		error("Bad input arguments.");
-		log("Format: sedlike <filename> <s1> <s2>");
+		log_format();
+		return (1);
+	}
+
+	std::string s1 = argv[2];
+	std::string s2 = argv[3];
+
+	if (s1 == "") {
+		error("<s1> must not be empty.");
+		log_format();
 		return (1);
 	}
 
 	std::ifstream input(argv[1]);
 	if (!input.is_open()) {
-		error("Couldn't open file.");
+		error("Couldn't open input file.");
 		return (1);
 	}
+
+	std::ostringstream buffer;
+	buffer << input.rdbuf();
+	std::string input_string = buffer.str();
+
+	std::string output_filename = std::string(argv[1]) + ".replace";
+	std::ofstream output(output_filename.c_str());
+
+	if (!output.is_open()) {
+		error("Couldn't create output file.");
+		return (1);
+	}
+
+	std::string output_string;
+	size_t cursor = 0;
+	while (true) {
+		size_t occurrence_idx = input_string.find(s1, cursor);
+		if (occurrence_idx == std::string::npos) {
+			output_string.append(input_string, cursor, std::string::npos);
+			break;
+		} else {
+			output_string.append(input_string, cursor, occurrence_idx - cursor);
+			output_string.append(s2);
+		}
+		cursor = occurrence_idx + s1.length();
+	}
+
+	output << output_string;
+
+	input.close();
+	output.close();
 }
