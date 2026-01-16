18864b79aad213c7e68740020adb8275aa425f68
Charlie-Steer <gruiz.carlos@gmail.com>
2026-01-16 13:56:57 +0100

    Added copy assignment operator.

--- cpp02/ex03/Point.cpp
+++ cpp02/ex03/Point.cpp
@@ -10,6 +10,11 @@ Point::Point(const float x, const float y) : x(x), y(y) {
 Point::Point(const Point &other) : x(other.x), y(other.y) {
 }
 
+Point &Point::operator=(const Point& other) {
+	(void)other;
+	return *this;
+}
+
 Point::~Point() {
 }
 
